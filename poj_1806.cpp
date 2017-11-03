186a97bf05235a5084634ec64d655bf7bedee75c
Austin Keil <austin.c.keil@gmail.com>
2017-11-03 16:13:49 -0700

    Update poj_1806.cpp

--- poj_1806.cpp
+++ poj_1806.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : poj_1806.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <iostream>
 #include <vector>
